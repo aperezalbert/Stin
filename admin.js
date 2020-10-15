7be3da88c4c9c9684066a22c2485c4879105dea9
Adriana Perez Albert <adrianaperezalbert@gmail.com>
2020-10-14 23:32:04 -0300

    Cambios en diseño de admin.html, admin.js y admin.css

--- admin.js
+++ admin.js
@@ -8,7 +8,8 @@
       const stylegame = document.querySelector('input[name="style"]:checked').value;
       const categorygame = document.getElementById('category').value;
       const publishedgame= document.querySelector('input[name="published"]:checked').value =='true'
-      const data = { name: namegame, style: stylegame, category: categorygame,publicado:publishedgame,destacado:'no'};
+      const imagengame=document.getElementById('imagen').value;
+      const data = { name: namegame, style: stylegame, category: categorygame,publicado:publishedgame,destacado:'no',imgSource:imagengame};
       postNewTodo(data);
       
     })
@@ -46,13 +47,14 @@
         </td>
         
         <td>
-        <button type="button" class="deleteButton btn text-white">Borrar</button>
+        <button type="button" class="btndelete text-white"><span class="fas fa-trash-alt"></span>  Borrar</button>
         </td>
         <td>
-        <button type="button" class=" btn text-white">Editar</button>
+        <button type="button" class="btnedit text-white"><span class="fas fa-edit"></span>
+         Editar</button>
         </td>
         <td>
-        <button type="button" class=" btn text-white">Destacado</button>
+        <button type="button" class="btndestacado text-white"><span class="fas fa-star"></span>  Destacar</button>
         </td>
       `
         todoList.appendChild(row);
@@ -61,7 +63,7 @@
     }
     
       
-       async function postNewTodo({ name,style,category,publicado,destacado}) {
+       async function postNewTodo({ name,style,category,publicado,destacado,imgSource}) {
        const url = 'http://localhost:3000/Games';
        const response = await fetch(url, {
         method: 'POST',
@@ -70,7 +72,7 @@
           'Content-Type': 'application/json'
         },
        
-        body: JSON.stringify({ name,style,category,publicado,destacado})
+        body: JSON.stringify({ name,style,category,publicado,destacado,imgSource})
       })
       const newData = await response.json();
       console.log(newData);
